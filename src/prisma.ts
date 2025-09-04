0ad00e3e0c9f7f7c161634482c1249a498d6350e
rizkirisjad <127320212+rizkirisjad@users.noreply.github.com>
2025-09-04 16:05:56 +0700

    y

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,3 +1,3 @@
-import { PrismaClient } from "prisma/generated/prisma";
+import { PrismaClient } from "../prisma/generated/prisma";
 
 export default new PrismaClient({ log: ["query", "info", "warn", "error"] });
